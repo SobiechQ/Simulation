2445ed5271c3ec3b97c4f5753ab5f46e52548247
Mikołaj <mikolajmikolaj090302@gmail.com>
2024-03-10 18:03:24 +0100

    simple smoke to rework

---
+++ src/main/java/Elements/Particles/SmokeParticle.java
@@ -0,0 +1,9 @@
+package Elements.Particles;
+
+import Elements.Api.Particle;
+
+public class SmokeParticle extends Particle {
+    public SmokeParticle() {
+        super(-0.2, 0.2, 0.2, 0.4, 30, 100);
+    }
+}
